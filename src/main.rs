7a7edbbeef84f3893e53dbc480bf4772b5bc33a0
Chris Johnson <christophere.johnson@theygsgroup.com>
2024-07-02 00:02:31 -0400

    adds updates
    sill

--- src/main.rs
+++ src/main.rs
@@ -10,77 +10,33 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ) {
-
-
-
     let mut grid = Grid {
-        size: Vec2::new(20.0, 20.0),
-        cell_size: Vec2::new(32.0, 32.0),
+        size: Vec2::new(5.0, 5.0),
+        cell_size: Vec2::new(16.0, 16.0),
     };
 
     commands.insert_resource(grid);
 
    
 
-    let player_texture = asset_server.load("Character.png");
+    let player_texture: Handle<Image> = asset_server.load("Character.png");
     let player_layout = TextureAtlasLayout::from_grid(Vec2::new(16.0, 16.0), 7, 2, None, None);
 
     let player_texture_atlas_layout = texture_atlas_layouts.add(player_layout);
-    let starting_point = grid.grid_to_world(IVec2::new(10, 10));
+    let starting_point = grid.grid_to_world(IVec2::new(1, 1));
 
     commands.spawn((Camera2dBundle::default(), PanCam::default(), MainCamera));
-
-    info!("{:?}", starting_point);
     
     commands.spawn((
         SpriteSheetBundle {
-            texture: player_texture,
-            atlas: TextureAtlas {
-                layout: player_texture_atlas_layout,
-                index: 1,
-            },
-            sprite: Sprite {
-                custom_size: Some(Vec2::splat(32.0)),
-                ..default()
-            },
-            transform: Transform::from_xyz(starting_point.x, starting_point.y, 1.0),
+           sprite: Sprite {
+            custom_size: Some(Vec2::splat(16.0)),
+            ..default()
+           },
             ..default()
         },
         Player,
     ));
-}
-
-fn draw_grid(
-    grid: Res<Grid>,
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
-
-    let texture: Handle<Image> = asset_server.load("Tileset.png");
-
-
-    let layout = TextureAtlasLayout::from_grid(Vec2::new(16.0, 16.0), 1, 1, None, None);
-
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
-
-    for x in 0..grid.size.x as i32 {
-        for y in 0..grid.size.y as i32 {
-            commands.spawn(SpriteSheetBundle {
-                texture: texture.clone(),
-                atlas: TextureAtlas {
-                    layout: texture_atlas_layout.clone(),
-                    index: 1,
-                },
-                transform: Transform::from_translation(Vec3::new(
-                    x as f32 * grid.cell_size.x,
-                    y as f32 * grid.cell_size.y,
-                    -1.0,
-                )),
-                ..Default::default()
-            });
-        }
-    }
 }
 
 fn player_input(
@@ -142,14 +98,14 @@ fn camera_follow (
 
 fn main() {
     App::new()
-        // .insert_resource(ClearColor(Color::GREEN))
+        .insert_resource(ClearColor(Color::GREEN))
         .insert_resource(LastMoveTime(0.0))
         .add_plugins((
             DefaultPlugins,
             WorldInspectorPlugin::new(),
             PanCamPlugin::default(),
         ))
-        .add_systems(Startup, (setup, draw_grid).chain())
+        .add_systems(Startup, (setup).chain())
         .add_systems(Update, (player_input, snap_to_grid, camera_follow).chain())
         .run();
 }
