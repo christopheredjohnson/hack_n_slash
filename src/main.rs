a77a75ad82a390bc41f3d69ebd8239ba702f0036
Chris Johnson <christophere.johnson@theygsgroup.com>
2024-07-04 19:20:33 -0400

    added animation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,78 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
+
+
+
+#[derive(Component)]
+struct AnimationIndices {
+    first: usize,
+    last: usize,
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+
+#[derive(Component)]
+struct Player;
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture = asset_server.load("mHero_.png");
+    let layout = TextureAtlasLayout::from_grid(Vec2::splat(24.0), 8, 6, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+
+
+    // Use only the subset of sprites in the sheet that make up the idle animation
+    let animation_indices = AnimationIndices { first: 0, last: 3 };
+
+
+
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_scale(Vec3::splat(24.0)),
+            texture,
+            ..default()
+        },
+        TextureAtlas {
+            layout: texture_atlas_layout,
+            index: animation_indices.first,
+        },
+        animation_indices,
+        AnimationTimer(Timer::from_seconds(0.125, TimerMode::Repeating)),
+        Player
+    ));
+}
+
+
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<(&AnimationIndices, &mut AnimationTimer, &mut TextureAtlas)>,
+) {
+    for (indices, mut timer, mut atlas) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            atlas.index = if atlas.index == indices.last {
+                indices.first
+            } else {
+                atlas.index + 1
+            };
+        }
+    }
+}
+
+
 
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (animate_sprite).chain())
         .run();
 }
